9300979c83c0e9c90f5994fcb445bb03fc293d46
liberatrice bayizere <92096502+liberatricebaizer@users.noreply.github.com>
2023-02-11 22:05:53 +0200

    Update Booking.jsx

--- src/container/booking/Booking.jsx
+++ src/container/booking/Booking.jsx
@@ -3,7 +3,7 @@ import "./Booking.css";
 const Booking = () => {
   return (
     <div className="booking">
-      <h3>hi there know it's working guys</h3>
+      <h3>hi there know it's working guys this me</h3>
     </div>
   );
 };
