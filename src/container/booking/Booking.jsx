c3f8611022dc5d2a8097c3282fe409de9d5a1706
tertcoder <82041184+tertcoder@users.noreply.github.com>
2023-02-13 21:45:10 +0200

    starting with booking section

--- src/container/booking/Booking.jsx
+++ src/container/booking/Booking.jsx
@@ -1,10 +1,20 @@
 import { Fragment } from "react";
 import "./Booking.css";
+import { FaSearch } from "react-icons/fa";
 
 const Booking = () => {
   return (
     <Fragment>
       <div className="booking">
+        <div className="booking__search">
+          <div className="booking__search--content">
+            <input type="text" name="searchHotel" id="searchHotel" />
+            <button>
+              <FaSearch />
+              {/* <span>Srch</span> */}
+            </button>
+          </div>
+        </div>
         <div className="booking__content">
           <div className="booking__hotel--current"></div>
           <div className="booking__hotels"></div>
