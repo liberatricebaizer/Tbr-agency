28c1fbfbdef1e729beac151417043332b828134e
liberatrice bayizere <92096502+liberatricebaizer@users.noreply.github.com>
2023-02-13 10:23:48 +0200

    new uptodate

--- src/container/booking/Booking.jsx
+++ src/container/booking/Booking.jsx
@@ -3,8 +3,11 @@ import "./Booking.css";
 const Booking = () => {
   return (
     <div className="booking">
-      <h3>hi there know it's working guys i'm me</h3>
-      <h2>i use react hehe</h2>
+      <div className="booking-form">
+        <form>
+          <input />
+        </form>
+      </div>
     </div>
   );
 };
